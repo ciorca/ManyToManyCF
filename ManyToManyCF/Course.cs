668603684feb731d4fe80924ba19be99aa9148af
ciorca <ciorca.gaby@gmail.com>
2022-06-15 18:08:12 +0300

    Add project files.

---
+++ ManyToManyCF/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManyToManyCF
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public IList<Student> Students { get; set; }
+
+
+    }
+}
